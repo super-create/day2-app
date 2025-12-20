d4a5c173bcc5cf57b4afbcdd3032b39152d08df0
super-create <harrythecopywiz@gmail.com>
2025-12-20 19:42:05 +0300

    Update homepage title to include test message

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function HomePage() {
   return (
     <div>
-      <h1>Home page</h1>
+      <h1>Home page this is a test</h1>
       <Link href="/about">Go to About</Link>
     </div>
   )
