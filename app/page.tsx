ba2f2a6cd27e6c851755c9f380fc4a589d84add7
super-create <harrythecopywiz@gmail.com>
2025-12-22 19:15:48 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,18 @@
-import Link from 'next/link'
-
-export default function HomePage() {
-  return (
-    <div>
-      <h1>Home page this is a test</h1>
-      <Link href="/about">Go to About</Link>
-    </div>
-  )
-}
 'use client'
 
 import { useState } from 'react'
+import Link from 'next/link'
 
 export default function HomePage() {
+
   const [count, setCount] = useState(0)
 
   return (
+
+
     <div>
+      <h1>Home page this is a test</h1>
+      <Link href="/about">Go to About</Link>
       <h1>Counter</h1>
       <p>Count: {count}</p>
       <button onClick={() => setCount(count + 1)}>
@@ -24,4 +20,7 @@ export default function HomePage() {
       </button>
     </div>
   )
+
+
 }
+
